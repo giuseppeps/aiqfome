8e2fed35df5f718186566decf432ef996c3ddc1b
YukioVN <noma@alunos.utfpr.edu.br>
2021-04-16 02:47:55 -0300

    Changed querySelector of area2-form

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,10 +1,10 @@
-const searchForm = document.querySelector('form');
+const searchForm = document.querySelector('.area2-form');
 const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('.container');
 let searchQuery = '';
 const APP_ID = "b05c8cce";
 const APP_key = "cb7ac662b9de5a8c8667ca5806216020";
-// console.log(container)
+
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector('input').value;
@@ -13,10 +13,10 @@ searchForm.addEventListener('submit', (e) => {
 
 async function fetchAPI(){
   const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
-  const response = await fetch(baseURL); 
-  const data = await response.json();
-  generateHTML(data.hits)
-  console.log(data);
+  const resp = await fetch(baseURL); 
+  const dataAPI = await resp.json();
+  generateHTML(dataAPI.hits)
+  console.log(dataAPI);
 }
 
 function generateHTML(results){
