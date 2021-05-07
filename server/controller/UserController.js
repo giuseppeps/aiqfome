bc35cdcd13a524e4bd47e88b0711fa5e5acad38a
YukioVN <noma@alunos.utfpr.edu.br>
2021-05-07 01:32:09 -0300

    Added authentication middleware

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
   async register(req, res) {
@@ -15,5 +16,21 @@ module.exports = {
     catch(err) {
       return res.status(400).send({error: 'Falha no cadastro'});
     }
+  },
+
+  async authenticate(req, res) {
+    const{email, password} = req.body;
+    
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user) {
+      return res.status(400).send({error: 'Usuário não encontrado'})
+    }
+
+    if(!await bcrypt.compare(password, user.password)) {
+      return res.status(400).send({error: 'Senha inválida'});
+    }
+
+    res.send({user});
   }
 };
