5742f4164945ce1139a9df26bb0b778f47db939b
YukioVN <noma@alunos.utfpr.edu.br>
2021-05-07 00:23:09 -0300

    Added register middleware

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,1 +1,19 @@
 const User = require('../model/User');
+
+module.exports = {
+  async register(req, res) {
+    const {email, password} = req.body;
+    try {
+      if(await User.findOne({email})) {
+        return res.status(400).send({error: 'Usuário existente'});
+      }
+
+      const user = await User.create(req.body);
+      return res.send({user});
+
+    }
+    catch(err) {
+      return res.status(400).send({error: 'Falha no cadastro'});
+    }
+  }
+};
