8915efbf1e0ab0883f3ab90c4949cd33ed56ced6
YukioVN <noma@alunos.utfpr.edu.br>
2021-05-07 01:06:28 -0300

    Added bcryptjs for password

--- server/model/User.js
+++ server/model/User.js
@@ -1,4 +1,5 @@
 let mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -12,5 +13,11 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Campo obrigatório"]
   }
 }, {timestamps: true});
+
+UserSchema.pre('save', async function(next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
 
 module.exports = mongoose.model('User', UserSchema);
