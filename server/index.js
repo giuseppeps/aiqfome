a902763fbeab5b8970fb7142d3962f65442f7868
YukioVN <noma@alunos.utfpr.edu.br>
2021-05-09 21:38:30 -0300

    Configured and imported express-session and connect-mongodb-session

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express  = require('express');
 const mongoose = require('mongoose');
-const router   = require('./routes');
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const cors = require('cors');
+const router = require('./routes');
 
 const app = express();
 const Port = process.env.Port || 3030;
 
-const Uri = 'mongodb+srv://dbUser:dbUser@clusteraiq.pe7ab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const Uri = 'mongodb+srv://dbUser:dbUser@clusteraiq.pe7ab.mongodb.net/myFirstDatabase';
+
+const store = new MongoDBStore({
+  uri: Uri,
+  collection: "sessions",
+});
 
 const connectDB = async () => {
   await mongoose.connect(Uri, 
@@ -19,7 +27,23 @@ const connectDB = async () => {
 }
 
 connectDB();
+
 app.use(express.json());
+app.use(cors());
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      secure: false,
+      expires: false
+    },
+  })
+);
+
 app.use(router);
 
 app.listen(Port, () => console.log(`Servidor na porta ${Port}`));
