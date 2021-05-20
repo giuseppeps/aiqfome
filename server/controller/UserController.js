b55564b38f3b492da73c2eb8a4c35c35ac6cb6ba
giuseppeps <giuseppe_000@hotmail.com>
2021-05-20 02:51:31 -0300

    [Added] functionalities for get and post recipes

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../model/User');
+const Post = require('../model/Post');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
@@ -42,5 +43,26 @@ module.exports = {
       });
       res.status(200).send('Usuário Deslogado');
     }
-  }
+  },
+
+  async post(req, res) {
+    const posts = await Post.find().sort('-createdAt');
+    return res.json(posts);
+  },
+  
+  async insertPosts(req, res) {
+    const {title, urlImage, urlRecipe} = req.body;
+    try {
+      if(await Post.findOne({title})) {
+        return res.status(409).send({error: 'Receita já existe'});
+      }
+
+      const posts = await Post.create(req.body);
+      return res.send({posts});
+
+    }
+    catch(err) {
+      return res.status(400).send({error: 'Falha no cadastro'});
+    }
+  },
 };
