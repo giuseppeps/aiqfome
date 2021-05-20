84f1eb32fe7061aea770f5820eb61332e2dd22e4
giuseppeps <giuseppe_000@hotmail.com>
2021-05-20 02:52:34 -0300

    [Featured] Schema of posts

---
+++ server/model/Post.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String, 
+    required: [true, "Campo obrigatório"],
+    unique: true,
+  },
+  urlImage: {
+    type: String, 
+    required: [true, "Campo obrigatório"]
+  },
+  urlRecipe: {
+    type: String, 
+    required: [true, "Campo obrigatório"]
+  } 
+}, {timestamps: true});
+
+module.exports = mongoose.model('posts', PostSchema);
