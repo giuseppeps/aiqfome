dcb7583d8886ca311c3110bff987de959c9971d7
giuseppeps <giuseppe_000@hotmail.com>
2021-05-21 02:49:52 -0300

    [Feature] Middleware

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const middleware = require('./middleware/isAdmin');
 const UserController = require('./controller/UserController');
 const router = express.Router();
 
 router.post("/signup", UserController.register);
-router.post("/login", UserController.login);
+router.post("/login", UserController.login, middleware.isAdmin);
 router.post("/logout", UserController.logout);
 router.get("/post", UserController.post);
 router.post("/insertPosts", UserController.insertPosts);
